3f648c5cb3569b4043ccd9c0a497b630dcb944b8
Hilson <hilsonxhero.me@gmail.com>
2023-05-31 19:43:13 +0330

    chore : update oauth

--- Modules/Auth/Http/Controllers/v1/App/Auth/GithubAuthController.php
+++ Modules/Auth/Http/Controllers/v1/App/Auth/GithubAuthController.php
@@ -4,9 +4,12 @@ namespace Modules\Auth\Http\Controllers\v1\App\Auth;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Modules\User\Entities\User;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cookie;
 use Laravel\Socialite\Facades\Socialite;
 use GuzzleHttp\Exception\ClientException;
+use Modules\User\Events\UserAuthenticatied;
 
 class GithubAuthController extends Controller
 {
@@ -19,10 +22,40 @@ class GithubAuthController extends Controller
     public function callback(Request $request)
     {
         try {
-            $user = Socialite::driver("github")->stateless()->user();
+            $auth_user = Socialite::driver("github")->stateless()->user();
         } catch (ClientException $exception) {
             return response()->json(['error' => 'Invalid credentials provided.'], 422);
         }
-        return $user;
+
+        $user = User::where('email', $auth_user->email)->first();
+
+        if (!$user) {
+            $user = User::create([
+                'username' => $auth_user->nickname,
+                'email' => $auth_user->email,
+            ]);
+        }
+
+        $token = $user->createToken('accessToken')->accessToken;
+
+        Cookie::queue(
+            'access_token',
+            $token,
+            45000,
+            null,
+            null,
+            false,
+            true,
+            false,
+            'Strict'
+        );
+
+        $request->headers->add([
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        event(new UserAuthenticatied($user));
+
+        return $token;
     }
 }
